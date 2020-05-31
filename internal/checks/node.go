539fc85be683d425e89792f528a131aa58a76caf
Nick Schuch <nick@myschuch.com>
2020-06-01 09:29:33 +1000

    Adds 5 minute warmup for Nodes to become Ready

--- internal/checks/node.go
+++ internal/checks/node.go
@@ -2,6 +2,7 @@ package checks
 
 import (
 	"fmt"
+	"time"
 
 	corev1 "k8s.io/api/core/v1"
 	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
@@ -17,7 +18,15 @@ func NodeStatus(clientset kubernetes.Interface) ([]Issue, error) {
 		return list, err
 	}
 
+	// Allows for new Nodes to become Ready without triggering alerts.
+	warmup := metav1.NewTime(time.Now().UTC().Add(-5 * time.Minute))
+
 	for _, node := range nodes.Items {
+		if !node.ObjectMeta.CreationTimestamp.Before(&warmup) {
+			// This Node is new and is still becoming Ready.
+			continue
+		}
+
 		for _, condition := range node.Status.Conditions {
 			if condition.Type == corev1.NodeReady && condition.Status == corev1.ConditionFalse {
 				list = append(list, Issue{
